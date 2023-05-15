c40f0d3cec36e72750111ad8813522a4aaee63f7
MayconCoutinho <mayconcoutinhoart@gmail.com>
2023-05-15 17:07:43 -0300

    refactor: corrindo error do .env modificando migrations e baseDataBase

--- src/database/migrations/Migrations.ts
+++ src/database/migrations/Migrations.ts
@@ -1,25 +1,33 @@
 import { BaseDataBase } from "../BaseDataBase";
-import products from "../../json/products.json"
+import products from "../../json/products.json";
 
-export const USER_LIST = "user"
-export const PRODUCTS_LIST = "products"
-export const PRODUCTS_USER_LIST = "products_user"
+export const USER_LIST = "user";
+export const PRODUCTS_LIST = "products";
+export const PRODUCTS_USER_LIST = "products_user";
 
-class Migrations extends BaseDataBase{
-    execute = async() => {
-        try{
-            console.log("Creating Tables...")
-            await this.createTables()
-            console.log("Tables created successfully")
-            console.log("Adding products...")
-            await Migrations.connection(PRODUCTS_LIST).insert(products)
-            console.log("Products added successfully")
-        } catch (error:any) {
-            console.log(error.mensage = undefined ? `Deu algum problema no banco de dados` : `Os produtos já existem no banco de dados` )
-        }
+class Migrations extends BaseDataBase {
+  execute = async () => {
+    try {
+      console.log("Creating tables...");
+      await this.createTables();
+      console.log("✅ Tables created successfully.");
+      console.log("✅ Populating tables...");
+      await this.insertData();
+      console.log("✅ Tables populated successfully.");
+      console.log("✅ Migrations completed.");
+    } catch (error) {
+      console.log("❌ FAILED! Error in migrations...");
+      if (error instanceof Error) {
+        console.log(error.message);
+      }
+    } finally {
+      console.log("✅ Ending connection...");
+      BaseDataBase.connection.destroy();
+      console.log("✅ Connection closed");
     }
-    createTables = async() => {
-        await BaseDataBase.connection.raw(`
+  };
+  createTables = async () => {
+    await BaseDataBase.connection.raw(`
 
         CREATE TABLE IF NOT EXISTS products(
             id INT PRIMARY KEY,
@@ -40,8 +48,11 @@ class Migrations extends BaseDataBase{
             quantity INT NOT NULL,
             FOREIGN KEY (id_user) REFERENCES user (id),
             FOREIGN KEY (id_product) REFERENCES products (id));
-        `)
-    }
+        `);
+  };
+  insertData = async () => {
+    await BaseDataBase.connection(PRODUCTS_LIST).insert(products);
+  };
 }
-const migrations = new Migrations()
-migrations.execute()
+const migrations = new Migrations();
+migrations.execute();
